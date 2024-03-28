61b908c5090ed0fac459af5a1ece7ed1727bd6cd
aksenmi <aksendev@gmail.com>
2024-03-28 11:47:35 +0900

    수정

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import useThemeStore from "@zustand/themeStore.mjs";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactCsspin } from "react-csspin";
+import "react-csspin/dist/style.css";
 import { Suspense } from "react";
 
 // react-query 사용
