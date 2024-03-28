c0e830e72b70ac2acd8806f075bb10f374dda9c6
aksenmi <aksendev@gmail.com>
2024-03-28 11:23:04 +0900

    게시판 완성

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,49 +1,72 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import { memberState } from "@recoil/user/atoms.mjs";
 import { Link } from "react-router-dom";
 import { useRecoilState } from "recoil";
-import Button from '@components/Button';
-import Theme from '@components/Theme';
+import Button from "@components/Button";
+import Theme from "@components/Theme";
 
-function Header(){
+function Header() {
   const navigate = useNavigate();
 
   const handleLogout = () => {
     setUser(null);
-    navigate('/');
+    navigate("/");
   };
 
   const [user, setUser] = useRecoilState(memberState);
 
   return (
-    <header className="min-w-80 bg-gray-200 dark:bg-gray-600 text-gray-800 dark:text-gray-200 transition-color duration-500 ease-in-out">
-      <nav className="flex flex-wrap justify-center items-center p-4 md:flex-nowrap md:justify-between">
-        <div className="w-1/2 order-1 md:w-auto">
+    <header className="text-gray-800 duration-500 ease-in-out bg-gray-200 min-w-80 dark:bg-gray-600 dark:text-gray-200 transition-color">
+      <nav className="flex flex-wrap items-center justify-center p-4 md:flex-nowrap md:justify-between">
+        <div className="order-1 w-1/2 md:w-auto">
           <a className="flex items-center gap-2" href="/">
-            <img className="mr-3 h-6 sm:h-9" src="/vite.svg" alt="로고 이미지" />
+            <img
+              className="h-6 mr-3 sm:h-9"
+              src="/vite.svg"
+              alt="로고 이미지"
+            />
             <span className="text-xl font-semibold">게시판</span>
           </a>
         </div>
-        <div className="w-auto order-2 text-lg mt-4 md:mt-0">
+        <div className="order-2 w-auto mt-4 text-lg md:mt-0">
           <ul className="flex items-center gap-6 uppercase">
-            <li><Link to="/boards">정보공유</Link></li>
-            <li><Link to="/boards">자유게시판</Link></li>
-            <li><Link to="/boards">질문게시판</Link></li>
+            <li>
+              <Link to="/boards">정보공유</Link>
+            </li>
+            <li>
+              <Link to="/boards">자유게시판</Link>
+            </li>
+            <li>
+              <Link to="/boards">질문게시판</Link>
+            </li>
           </ul>
         </div>
-        <div className="w-1/2 order-1 flex justify-end items-center md:order-2 md:w-auto">
-          { user ? (
+        <div className="flex items-center justify-end order-1 w-1/2 md:order-2 md:w-auto">
+          {user ? (
             <p className="flex items-center">
-              <img className="w-8 rounded-full mr-2" src={`https://market-lion.koyeb.app/api/files/${ user.profile }`}></img>
-              { user.name }님 :)
-              <Button size="sm" onClick={ handleLogout }>로그아웃</Button>
+              <img
+                className="w-8 mr-2 rounded-full"
+                src={`${import.meta.env.VITE_API_SERVER}/files/${user.profile}`}
+              ></img>
+              {user.name}님 :)
+              <Button size="sm" onClick={handleLogout}>
+                로그아웃
+              </Button>
             </p>
           ) : (
             <div className="flex justify-end">
-              <Button size="sm" onClick={ () => navigate('/users/login') }>로그인</Button>
-              <Button size="sm" bgColor="gray" onClick={ () => navigate('/users/signup') }>회원가입</Button>
+              <Button size="sm" onClick={() => navigate("/users/login")}>
+                로그인
+              </Button>
+              <Button
+                size="sm"
+                bgColor="gray"
+                onClick={() => navigate("/users/signup")}
+              >
+                회원가입
+              </Button>
             </div>
-          ) }
+          )}
           <Theme />
         </div>
       </nav>
--- src/pages/board/BoardList.jsx
+++ src/pages/board/BoardList.jsx
@@ -11,15 +11,22 @@ import { useRecoilValue } from "recoil";
 
 // import { useEffect, useState } from "react";
 
-function BoardList(){
+function BoardList() {
   const axios = useCustomAxios();
   // /posts?page=3
   const [searchParams, setSearchParams] = useSearchParams();
 
   const { isLoading, data, error, refetch } = useQuery({
-    queryKey: ['posts'],
-    queryFn: () => axios.get('/posts', { params: { page: searchParams.get('page'), limit: 3, keyword: searchParams.get('keyword') } }),
-    select: response => response.data,
+    queryKey: ["posts"],
+    queryFn: () =>
+      axios.get("/posts", {
+        params: {
+          page: searchParams.get("page"),
+          limit: import.meta.env.VITE_POST_LIMIT,
+          keyword: searchParams.get("keyword"),
+        },
+      }),
+    select: (response) => response.data,
     // staleTime: 1000*100, // 쿼리 실행 후 캐시가 유지되는 시간(기본, 0)
     suspense: true,
   });
@@ -30,35 +37,41 @@ function BoardList(){
 
   // 검색 요청시 주소의 query string 수정
   const handleSearch = (keyword) => {
-    searchParams.set('keyword', keyword);
-    searchParams.set('page', 1);
+    searchParams.set("keyword", keyword);
+    searchParams.set("page", 1);
     setSearchParams(searchParams);
   };
 
   const user = useRecoilValue(memberState);
   const navigate = useNavigate();
   const handleNewPost = () => {
-    if(!user){
-      const gotoLogin = confirm('로그인 후 이용 가능합니다.\n로그인 페이지로 이동하시겠습니까?');
-      gotoLogin && navigate('/users/login');
-    }else{
+    if (!user) {
+      const gotoLogin = confirm(
+        "로그인 후 이용 가능합니다.\n로그인 페이지로 이동하시겠습니까?"
+      );
+      gotoLogin && navigate("/users/login");
+    } else {
       navigate(`/boards/new`);
     }
-  }
+  };
 
-  const itemList = data?.item?.map(item => <BoardListItem key={ item._id } item={ item } />);
+  const itemList = data?.item?.map((item) => (
+    <BoardListItem key={item._id} item={item} />
+  ));
 
   return (
-    <div className="min-w-80 p-4">
-      <div className="text-center py-4">
-        <h2 className="text-2xl font-bold text-gray-700 dark:text-gray-200">게시물 목록 조회</h2>
+    <div className="p-4 min-w-80">
+      <div className="py-4 text-center">
+        <h2 className="text-2xl font-bold text-gray-700 dark:text-gray-200">
+          게시물 목록 조회
+        </h2>
       </div>
       <div className="flex justify-end mr-4">
-        <Search onClick={ handleSearch } />
-        <Button onClick={ handleNewPost }>글쓰기</Button>
+        <Search onClick={handleSearch} />
+        <Button onClick={handleNewPost}>글쓰기</Button>
       </div>
       <section className="p-4">
-        <table className="border-collapse w-full table-fixed">
+        <table className="w-full border-collapse table-fixed">
           <colgroup>
             <col className="w-[10%] sm:w-[10%]" />
             <col className="w-[60%] sm:w-[40%]" />
@@ -67,28 +80,38 @@ function BoardList(){
             <col className="w-0 sm:w-[25%]" />
           </colgroup>
           <thead>
-            <tr className="border-b border-solid border-gray-200">
+            <tr className="border-b border-gray-200 border-solid">
               <th className="p-2 whitespace-nowrap">번호</th>
               <th className="p-2 whitespace-nowrap">제목</th>
               <th className="p-2 whitespace-nowrap">글쓴이</th>
-              <th className="p-2 whitespace-nowrap hidden sm:table-cell">조회</th>
-              <th className="p-2 whitespace-nowrap hidden sm:table-cell">작성일</th>
+              <th className="hidden p-2 whitespace-nowrap sm:table-cell">
+                조회
+              </th>
+              <th className="hidden p-2 whitespace-nowrap sm:table-cell">
+                작성일
+              </th>
             </tr>
           </thead>
           <tbody>
-            { isLoading && (
-              <tr><td colSpan="5">로딩중...</td></tr>
-            ) }
-            { error && (
-              <tr><td colSpan="5">{ error.message }</td></tr>
-            ) }
-            { itemList }
-
+            {isLoading && (
+              <tr>
+                <td colSpan="5">로딩중...</td>
+              </tr>
+            )}
+            {error && (
+              <tr>
+                <td colSpan="5">{error.message}</td>
+              </tr>
+            )}
+            {itemList}
           </tbody>
         </table>
-        <hr/>
+        <hr />
 
-        <Pagination totalPage={ data?.pagination.totalPages } current={ data?.pagination.page } />
+        <Pagination
+          totalPage={data?.pagination.totalPages}
+          current={data?.pagination.page}
+        />
       </section>
     </div>
   );
